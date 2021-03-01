6e41428638beb2ec5639e692fb951e04217e7e53
haris0 <hari.surrisyad@gmail.com>
2021-03-01 13:52:53 +0700

    load source data from public News API

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,21 +3,30 @@
 </template>
 
 <script>
+  let self;
   import HeaderNav from '@/components/HeaderNav.vue'
+  import {getHeadlines} from '@/services'
   export default {
     name: 'Home',
-
+    created(){
+      self = this;  
+    },
     components: {
       HeaderNav
     },
     mounted(){
+      self.fetchHeadlines()
     },
     data:()=>({
+      headlines : []
     }),
     computed: {
-     
     },
     methods:{
+      fetchHeadlines : async() => {
+        const response = await getHeadlines(10, 1);
+        console.log(response)
+      }
     }
   }
 </script>
---
+++ src/services/index.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const apiKey = "bf902b2ca9db4c02b37cb565501b3167"
+const headlines = "top-headlines"
+
+const axiosInstance = axios.create({
+  baseURL: 'http://newsapi.org/v2/',
+  timeout: 3000,
+});
+
+export const getHeadlines = async (pageSize, page) =>{
+  try {
+    const response = await axiosInstance.get(headlines,{
+      params :{
+        pageSize:pageSize,
+        page:page,
+        country:"id",
+        apiKey:apiKey,
+      }
+    });
+    return response
+  } catch (error) {
+    return error
+  }
+}
